8f4254edc91e5907bb2f3b84a2029e069c2c3bbc
Beatrice Severance <bms0068@auburn.edu>
2020-04-07 18:09:26 -0500

    cleaned up TestVetCodon regexp

--- find_orf.py
+++ find_orf.py
@@ -119,7 +119,7 @@ def vet_codon(codon):
     # Change `codon_pattern_str` so that it will match any valid codons, and
     # only valid codons.
     # Read the docstring above for additional clues.
-    codon_pattern_str = r'[A|U|G|C|a|u|g|c][A|U|G|C|a|u|g|c][A|U|G|C|a|u|g|c]$'
+    codon_pattern_str = r'^[A|U|G|C|a|u|g|c]{3}$'
     ##########################################################################
 
     codon_pattern = re.compile(codon_pattern_str)
