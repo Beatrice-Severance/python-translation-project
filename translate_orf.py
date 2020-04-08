5e54600b767c5e99f1d7633a123d43fa95a114f6
Beatrice Severance <bms0068@auburn.edu>
2020-04-08 13:54:34 -0500

    cannot transition from find_first_orf to translate_sequence

--- translate_orf.py
+++ translate_orf.py
@@ -3,18 +3,10 @@
 import sys
 import re
 
-from find_orf import find_first_orf
-from translate import translate_sequence
-from find_orf import parse_sequence_from_path
-
 def main():
     import argparse
-
-    # Create a command-line parser object
     parser = argparse.ArgumentParser(
             formatter_class = argparse.ArgumentDefaultsHelpFormatter)
-
-    # Tell the parser what command-line arguments this script can receive
     parser.add_argument('sequence',
             metavar = 'SEQUENCE',
             type = str,
@@ -36,12 +28,7 @@ def main():
             nargs = '+', # one or more arguments
             default = ['UAA', 'UAG', 'UGA'],
             help = ('One or more possible stop codons.'))
-
- # Parse the command-line arguments into a 'dict'-like container
     args = parser.parse_args()
-
-    # Check to see if the path option was set to True by the caller. If so, parse
-    # the sequence from the path
     if args.path:
         sequence = parse_sequence_from_path(args.sequence)
     else:
@@ -51,8 +38,6 @@ def main():
             start_codons = args.start_codons,
             stop_codons = args.stop_codons)
     sys.stdout.write('{}\n'.format(orf))
-
-
 if __name__ == '__main__':
     genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
     rna_seq = ("AUG"
@@ -73,4 +58,12 @@ if __name__ == '__main__':
             "ACA"
             "GCG")
 
+from find_orf import find_first_orf
+from translate import translate_sequence
+from find_orf import parse_sequence_from_path
+
+value = input("Please enter a sequence:\n")
+find_first_orf(value)
+translate_sequence()
+parse_sequence_from_path()
 
